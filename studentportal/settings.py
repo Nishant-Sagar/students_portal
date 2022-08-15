a29d3f3cbb3a89049c709eb1f95867898009b254
Nishant-Sagar <nishantsagar045@gmail.com>
2022-08-15 13:00:37 +0530

    add static

--- studentportal/settings.py
+++ studentportal/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+# SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure-cty%rxjk3mm-%9lv9cnf!#0s%&_elphl9ln3e*eyh&eog)vx*)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
