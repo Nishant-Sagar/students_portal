90c75178597be5424e0b86c3f7bcbef9343616f3
Nishant-Sagar <nishantsagar045@gmail.com>
2022-08-15 12:41:04 +0530

    a

--- studentportal/settings.py
+++ studentportal/settings.py
@@ -127,8 +127,8 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # STATICFILES_DIRS = ['/home/nishant/Dashboard/static']
 LOGIN_REDIRECT_URL = 'home'
